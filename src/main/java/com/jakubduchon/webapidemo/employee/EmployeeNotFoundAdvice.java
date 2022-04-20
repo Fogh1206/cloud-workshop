592a843246109b39ef4b923dce8d1ede714cd1d6
Fogh1206 <esbenvensel.fogh@gmail.com>
2022-04-20 15:51:22 +0200

    Setup CI/CD

--- src/main/java/com/jakubduchon/webapidemo/employee/EmployeeNotFoundAdvice.java
+++ src/main/java/com/jakubduchon/webapidemo/employee/EmployeeNotFoundAdvice.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+// HELLO
+
 @ControllerAdvice
 public class EmployeeNotFoundAdvice {
     @ResponseBody
